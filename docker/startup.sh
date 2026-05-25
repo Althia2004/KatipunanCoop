dded846b1955018d103548c6283263715b7ce47f
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2026-05-26 01:35:36 +0800

    Updated docker/startup.sh.

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-# Cache configurations for speed
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+# Forcefully remove all cached configurations to stop silent failures
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
 
-# Run database migrations automatically
+# Run migrations (Safe, will say "Nothing to migrate")
 php artisan migrate --force
 
-# Start PHP-FPM in the background and Nginx in the foreground
-php-fpm -D && nginx -g "daemon off;"
+# Start Nginx & PHP-FPM
+nginx -g "daemon off;" &
+php-fpm
