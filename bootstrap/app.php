99a92f658bdf3a830a9dc4c1268d58ea24488881
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2026-05-26 02:12:27 +0800

    Add trustProxies to bootstrap app config.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies forwarded by Render's infrastructure
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->alias([
@@ -28,7 +31,6 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
             SetTeamUrlDefaults::class,
-
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
